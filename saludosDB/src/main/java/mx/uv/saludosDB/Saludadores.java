d789dde1ea81cfb1217e5a67b7afaaecc432afb7
AntonioJimenez01 <89932599+AntonioJimenez01@users.noreply.github.com>
2023-03-29 15:58:40 -0600

    Add files via upload

---
+++ saludosDB/src/main/java/mx/uv/saludosDB/Saludadores.java
@@ -0,0 +1,34 @@
+package mx.uv.saludosDB;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class Saludadores {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Integer id;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+}
